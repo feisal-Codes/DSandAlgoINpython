c7f15c7af7f72e6067b545a80cf09ad0b590398a
feisal-Codes <64246952+feisal-Codes@users.noreply.github.com>
2020-12-23 03:18:27 +0300

    Update linkedlist.py

--- linkedlist.py
+++ linkedlist.py
@@ -36,7 +36,59 @@ class Linkedlist:
         new_node.next=prev_node.next
         prev_node.next=new_node
 
+    def delete_node(self,key):
+        #deleting head 
+        #get the current head node(first node)
+        cur_node=self.head
+        #check if the data  to be deleted matches the current node's data(head node)
+        if cur_node and cur_node.data==key:
+            #point to the next node and set it as the header
+            #set the current node to be none
+            self.head=cur_node.next
+            cur_node=None
+            return
+        prev_node =None
+        while cur_node and cur_node.data != key:
+            prev_node=cur_node #get the previous node to the node we want to delete as ones the node's data matches the key we exit the loop and prev node
+                               #remains 
+            cur_node=cur_node.next #sets the cur_node to the next node #while loop depends on this to exit  
+        if cur_node is None:
+            return
+        prev_node.next=cur_node.next
+        cur_node=None
+    def delete_node_at_position(self,pos):
+        #deleting head 
+        #get the current head node(first node)
+        cur_node=self.head
+        #check if the data of the node to be deleted matches the current node(head node)
+        if pos==0:
+            #point to the next node and set it as the header
+            #set the current node to be none
+            self.head=cur_node.next
+            cur_node=None
+            return
+        prev_node =None
+        count=0
+        while cur_node and count !=0 :
+            prev_node=cur_node
+            cur_node=cur_node.next  
+            count+=1
+        if cur_node is None:
+            return
+        prev_node.next=cur_node.next
+        cur_node=None
         
+    def getLengthIterative(self):
+        count=0
+        cur_node=self.head
+        while cur_node:
+            count+=1
+            cur_node=cur_node.next
+        return count
+    def getLengthRecursively(self,node):
+        if node==None:
+            return 0
+        return 1 + self.getLengthRecursively(node.next)
             
 
 mylist = Linkedlist()
@@ -50,9 +102,9 @@ mylist.print_list()
 mylist.insert_after(mylist.head.next,'y')
 print('..............')
 mylist.print_list()
-
-
-
-
-
+print('..............')
+mylist.delete_node('l')
+mylist.print_list()
+print(mylist.getLengthIterative())
+print(mylist.getLengthRecursively(mylist.head))
 
